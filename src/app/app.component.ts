e4143ef9fd7b3f05e3771973c780900f14d54ab1
anmol0mishra@gmail.com <anmol0mishra@gmail.com>
2019-08-07 19:41:41 +0530

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo-App'; 
+  todos = [{label: 'Buy Milk ',
+  done: false,
+  priority: 3},
+  {label: 'Pay Mobile Bill ',
+  done: true,
+  priority: 1},
+  {label: 'Clean House',
+  done: false,
+  priority: 2},
+  {label: 'Fix the bulb',
+  done: false,
+  priority: 5
+  }
+];
+  addTodo(newTodoLabel){
+    var newTodo = {
+      label: newTodoLabel,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+  deleteTodo(todo){
+    this.todos = this.todos.filter(t => t.label != todo.label );
+  }
 }
